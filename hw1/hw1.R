7b77dd91189b115baa4abf08e3d6d432b3704ed6
Xuan Luu <xluu@berkeley.edu>
2015-03-06 11:08:09 -0800

    resubmission

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -75,6 +75,7 @@ ma##############################################################################
 ###         Session > Set Working Directory > To Source File Location
 
 # Load family.rda that is in your hw1 folder, the variable is called family:
+load("family.rda")
 
 ### Check what data type the variable is with : class(family)
 ### now take a look at the data frame by typing family in the console.
