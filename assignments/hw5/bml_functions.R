d016a74976a64c0aa0b71b4537f655e69fec5f64
Xuan Luu <xluu@berkeley.edu>
2015-03-15 23:16:28 -0700

    submit hw5

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,11 +8,15 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
+  num_cars = round(r * c * p)
+  rcars = round(num_cars/2)
+  bcars= num_cars - rcars
+  empty = (r * c) - num_cars
+  data = sample(c(rep(0,empty), rep(1, rcars), rep(2, bcars)))
+  m = matrix(data = data, nrow = r, ncol = c)
   
-   #return(m)
+  return(m)
 }
-
 #### Function to move the system one step (east and north)
 ## Input : a matrix [m] of the same type as the output from bml.init()
 ## Output : TWO variables, the updated [m] and a logical variable
@@ -21,10 +25,53 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
-bml.step <- function(m){
+bml.north <- function(c){ 
+  copy = c
+  for (i in 1:length(copy)) {    
+    if (i == 1) {
+      if (copy[i] == 2 && copy[length(c)] == 0) {
+        c[i] <- 0 
+        c[length(c)] <- 2
+      }
+    } else {
+      if (copy[i] == 2 && copy[i-1] == 0) {
+        c[i] <- 0 
+        c[i-1] <- 2
+      }
+    }
+  }
+  return (c)
+}
 
+bml.east <- function(r){ 
+  copy = r
+  for (i in 1:length(r)) {    
+    if (i == length(r)) {
+      if (copy[i] == 1 && copy[1] == 0) {
+        r[i] <- 0 
+        r[1] <- 1
+      }
+    } else {
+      if (copy[i] == 1 && copy[i+1] == 0) {
+        r[i] <- 0 
+        r[i+1] <- 1
+      }
+    }
+  }
+  return (r)
+}
+
+
+bml.step <- function(m){
+  old = m 
+  for (r in 1:nrow(m)) {
+    m[r,] = bml.east(m[r,])
+  }
+  for (c in 1:ncol(m)) {
+    m[,c] = bml.north(m[,c])
+  }
   
-   #return(list(m, grid.new))
+  return(list(m, isTRUE(all.equal(old,m))))
 }
 
 #### Function to do a simulation for a given set of input parameters
@@ -32,5 +79,14 @@ bml.step <- function(m){
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
 bml.sim <- function(r, c, p){
-
+  m = bml.init(r,c,p)
+  for (i in 1:500) {
+    x = bml.step(m)
+    if (x[[2]] == TRUE) {
+      return (FALSE)
+    } 
+  }
+  return (TRUE)
+  
+  
 }
