d80a29b9b68c88e2d9670f93cc39e36fda128a92
Xuan Luu <xluu@berkeley.edu>
2015-04-11 18:31:15 -0700

    submit hw6

--- hw6/hw6.r
+++ hw6/hw6.r
@@ -31,13 +31,30 @@ sim.doctors <- function(initial.doctors, n.doctors, n.days, p){
 
   # Set up the output variable, define it as a matrix then use initial.doctors
   # to set the first column (day)
-
+  has_adopted <- matrix(, nrow = n.doctors, ncol = n.days)
+  has_adopted[, 1] <- initial.doctors
+  
   # Run a simulation for <n.days> (use a for loop).  In the loop:
   # 1) pick two random doctors
   # 2) check if one has adopted the other hasn't
   # 3) convert the non-adopter with probability p
+  for (i in 2:n.days){
+    doctor <- sample(1:n.doctors, 2)
+    if ((has_adopted[doctor[1], i-1] == 1) && (has_adopted[doctor[2], i-1] == 0)){
+      result <- has_adopted[,i-1]
+      result[doctor[2]] <- sample(c(0,1), 1, replace=T, c(1-p, p))
+      has_adopted[, i] <- result
+    } else if ((has_adopted[doctor[2], i-1] == 1) && (has_adopted[doctor[1], i-1] == 0)){
+      result <- has_adopted[,i-1]
+      result[doctor[1]] <- sample(c(0,1), 1, replace=T, c(1-p, p))
+      has_adopted[, i] <- result
+    } else{
+      has_adopted[, i] <- has_adopted[, i-1]
+    }
+  }
 
   # return the output
+  return(has_adopted)
 
 }
 
@@ -51,3 +68,15 @@ set.seed(42)
 # on y-axis : the number of doctors that have already adopted the drug, on that day
 # Put all 5 lines in one figure (e.g. use first plot() then lines() for the subsequent lines)
 
+p1 = sim.doctors(sample(c(0,1), 100, c(0.9, 0.1), replace = T), 100, 100, 0.1)
+p2 = sim.doctors(sample(c(0,1), 100, c(0.9, 0.1), replace = T), 100, 100, 0.2)
+p3 = sim.doctors(sample(c(0,1), 100, c(0.9, 0.1), replace = T), 100, 100, 0.4)
+p4 = sim.doctors(sample(c(0,1), 100, c(0.9, 0.1), replace = T), 100, 100, 0.6)
+p5 = sim.doctors(sample(c(0,1), 100, c(0.9, 0.1), replace = T), 100, 100, 0.8)
+plot(x=1:ncol(p1), y=apply(p1, 2, sum), type ="l", xlab="days", 
+     ylab = "Number of doctors that adopted the drug",
+     main = "Simulation of period adopted drug", col='hotpink')
+lines(x=1:ncol(p2), y = apply(p2, 2, sum), col="blue")
+lines(x=1:ncol(p3), y = apply(p3, 2, sum), col="purple")
+lines(x=1:ncol(p4), y = apply(p4, 2, sum), col="red")
+lines(x=1:ncol(p5), y = apply(p5, 2, sum), col="green")
