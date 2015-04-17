b585491f3a667e6cee87bc62ab56082ba91a577c
Xuan Luu <xluu@berkeley.edu>
2015-04-17 12:06:57 -0700

    submit

--- hw5/bml_simulation.R
+++ hw5/bml_simulation.R
@@ -8,11 +8,59 @@
 #### how many steps you observered before concluding that it is in a free flowing state.
 
 
-counter = 0
-for (i in 1:100) {
-  if(!bml.sim(5,4,.9)) {
-    print (i)
-    counter = counter + 1
-  }
-}
-print(paste0("It gridlocked ", counter, " times"))
+source("bml_functions.r")
+#question1 & 2 (for observation)
+bml.sim(100,100,0.2)
+bml.sim(100,100,0.2)
+bml.sim(100,100,0.2)
+bml.sim(100,100,0.2)
+bml.sim(100,100,0.2)
+
+bml.sim(100,100,0.40)
+bml.sim(100,100,0.40)
+bml.sim(100,100,0.40)
+bml.sim(100,100,0.40)
+bml.sim(100,100,0.40)
+
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+
+
+#question 3
+bml.sim(100,100,0.35)
+bml.sim(100,100,0.35)
+bml.sim(100,100,0.35)
+bml.sim(100,100,0.35)
+bml.sim(100,100,0.35)
+
+bml.sim(100,100,0.38)
+bml.sim(100,100,0.38)
+bml.sim(100,100,0.38)
+bml.sim(100,100,0.38)
+bml.sim(100,100,0.38)
+
+bml.sim(100,100,0.39)
+bml.sim(100,100,0.39)
+bml.sim(100,100,0.39)
+bml.sim(100,100,0.39)
+bml.sim(100,100,0.39)
+
+#experiment
+bml.sim(1,1,0.3)
+bml.sim(1,1,0.3)
+bml.sim(1,1,0.3)
+
+bml.sim(2,2,0.3)
+bml.sim(2,2,0.3)
+bml.sim(2,2,0.3)
+
+bml.sim(10000,1,0.3)
+bml.sim(10000,1,0.3)
+bml.sim(10000,1,0.3)
+
+bml.sim(1,10000,0.3)
+bml.sim(1,10000,0.3)
+bml.sim(1,10000,0.3)
